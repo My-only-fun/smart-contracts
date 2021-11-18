b169250a60e291c6e2c70742fece92c219bb0f56
Gwendal <gwendou95@hotmail.fr>
2021-11-19 00:38:49 +0100

    ERC20 tokens creation

--- contracts/MyOnlyFun.sol
+++ contracts/MyOnlyFun.sol
@@ -1,6 +1,38 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
+import "./token/OnlyFunERC20.sol";
+
 contract MyOnlyFun {
+
+    // structures for token and ownership
+    address[] tokens;
+
+    // mapping of token to owner
+    mapping(address => address) owners;
     
+    // Create and deploy token
+    function createAndDeployOnlyFunERC20(
+        string calldata name,
+        string calldata symbol,
+        uint8 decimals,
+        uint256 initialSupply
+    ) external returns (OnlyFunERC20 creditsAddress) {
+
+        OnlyFunERC20 newToken = new OnlyFunERC20(
+            name,
+            symbol,
+            decimals,
+            initialSupply
+        );
+
+        tokens.push(address(newToken));
+        return newToken;
+    }
+
+    // get tokens
+    function getTokens() external view returns (address[] memory) {
+        return tokens;
+    }
+
 }
